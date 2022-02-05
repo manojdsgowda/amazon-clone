91d78c61078fab124243a6ca81f529e9043f72fb
manojdsgowda1 <manoj@grihasoft.com>
2022-02-05 19:44:22 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPOIaaiCZxK6q6uuOyMyiD8r0anr4hIgo",
+  authDomain: "challenge-cc026.firebaseapp.com",
+  projectId: "challenge-cc026",
+  storageBucket: "challenge-cc026.appspot.com",
+  messagingSenderId: "45958231398",
+  appId: "1:45958231398:web:5fdcd6435ce9bc7a20c4a7",
+  measurementId: "G-4K5CHZSSML",
+};
